0c6e33da48a9de6167603a8e5b4bacdaf4f812a2
user.arvind <arvindjavadeveloper@gmail.com>
2014-11-06 15:09:26 +0800

    With Adding TabView function

--- src/org/primefaces/showcase/view/button/ButtonView.java
+++ src/org/primefaces/showcase/view/button/ButtonView.java
@@ -7,16 +7,29 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import org.primefaces.component.tabview.Tab;
+import org.primefaces.component.tabview.TabView;
  
 @ManagedBean
 public class ButtonView {
-     
+	
     public void buttonAction(ActionEvent actionEvent) {   
         Tab tab = new Tab();
         tab.setTitle("Arvind");
-        tab.setRendered(true);
         UIViewRoot view = FacesContext.getCurrentInstance().getViewRoot();
-        UIComponent parent = view.findComponent("arvind");
-        tab.setParent(parent);
+        TabView parent = (TabView) view.findComponent("arvind");
+		for (UIComponent uiComponent : view.getChildren()) {
+			for (UIComponent uiComponent1 : uiComponent.getChildren()) {
+				for (UIComponent uiComponent2 : uiComponent1.getChildren()) {			
+					for (UIComponent uiComponent3 : uiComponent2.getChildren()) {
+						for (UIComponent uiComponent4 : uiComponent3.getChildren()) {
+							if (uiComponent4 instanceof TabView) {
+								parent = (TabView) uiComponent4;
+							}
+						}
+					}
+				}
+			}
+		}
+		parent.getChildren().add(tab);
     }
 }
